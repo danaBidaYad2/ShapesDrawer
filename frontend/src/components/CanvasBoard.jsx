9e8ee156e799c7666c328001c7fe8c61074f8f01
DanaBida <42296148+DanaBida@users.noreply.github.com>
2023-07-15 21:00:12 +0300

    fix react hooks from eslint

--- frontend/src/components/CanvasBoard.jsx
+++ frontend/src/components/CanvasBoard.jsx
@@ -1,12 +1,52 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect, useRef } from 'react';
 import { StyledCanvas } from '../styles/canvas.styles';
 import { AppContext } from './App';
 
 export const CanvasBoard = React.forwardRef((_, { contextRef, canvasRef }) => {
   const { color, shape } = useContext(AppContext);
-  let isDrawing = false,
-    startX,
-    startY;
+  const isDrawing = useRef(false);
+  const startX = useRef(0);
+  const startY = useRef(0);
+
+  const handleMouseDown = useCallback((event) => {
+    startX.current = event.offsetX;
+    startY.current = event.offsetY;
+    isDrawing.current = true;
+  }, []);
+
+  const handleMouseUp = useCallback(
+    (event) => {
+      if (!isDrawing.current) return;
+
+      let { offsetX, offsetY } = event;
+      let context = contextRef.current;
+
+      if (shape === 'line') {
+        context.beginPath();
+        context.moveTo(startX.current, startY.current);
+        context.lineTo(offsetX, offsetY);
+        context.stroke();
+      } else if (shape === 'triangle') {
+        context.beginPath();
+        context.moveTo(startX.current, startY.current);
+        context.lineTo(offsetX, offsetY);
+        context.lineTo(startX.current - (offsetX - startX.current), offsetY);
+        context.closePath();
+        context.stroke();
+      } else if (shape === 'rectangle') {
+        let width = offsetX - startX.current;
+        let height = offsetY - startY.current;
+        context.strokeRect(startX.current, startY.current, width, height);
+      } else if (shape === 'circle') {
+        let radius = Math.sqrt((offsetX - startX.current) ** 2 + (offsetY - startY.current) ** 2);
+        context.beginPath();
+        context.arc(startX.current, startY.current, radius, 0, 2 * Math.PI);
+        context.stroke();
+      }
+      isDrawing.current = false;
+    },
+    [shape, contextRef]
+  );
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -23,44 +63,7 @@ export const CanvasBoard = React.forwardRef((_, { contextRef, canvasRef }) => {
       canvas.removeEventListener('mousedown', handleMouseDown);
       canvas.removeEventListener('mouseup', handleMouseUp);
     };
-  }, [color, shape]);
-
-  const handleMouseDown = (event) => {
-    startX = event.offsetX;
-    startY = event.offsetY;
-    isDrawing = true;
-  };
-
-  const handleMouseUp = (event) => {
-    if (!isDrawing) return;
-
-    let { offsetX, offsetY } = event;
-    let context = contextRef.current;
-
-    if (shape === 'line') {
-      context.beginPath();
-      context.moveTo(startX, startY);
-      context.lineTo(offsetX, offsetY);
-      context.stroke();
-    } else if (shape === 'triangle') {
-      context.beginPath();
-      context.moveTo(startX, startY);
-      context.lineTo(offsetX, offsetY);
-      context.lineTo(startX - (offsetX - startX), offsetY);
-      context.closePath();
-      context.stroke();
-    } else if (shape === 'rectangle') {
-      let width = offsetX - startX;
-      let height = offsetY - startY;
-      context.strokeRect(startX, startY, width, height);
-    } else if (shape === 'circle') {
-      let radius = Math.sqrt((offsetX - startX) ** 2 + (offsetY - startY) ** 2);
-      context.beginPath();
-      context.arc(startX, startY, radius, 0, 2 * Math.PI);
-      context.stroke();
-    }
-    isDrawing = false;
-  };
+  }, [color, shape, handleMouseDown, handleMouseUp, contextRef, canvasRef]);
 
   return <StyledCanvas width={1000} height={600} ref={canvasRef} />;
 });
